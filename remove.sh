4ea55b6998af6135f5cb74755a731cb52c9ee84b
Florian Pinault <florian.pinault@meteo.fr>
2018-10-05 14:49:19 +0200

    minor change to install scripts

--- remove.sh
+++ remove.sh
@@ -5,7 +5,7 @@ echo 'To remove your conda environment'
 echo
 echo "1 - Delete your conda folders :"
 echo "   rm -rf $HOME/miniconda3"
-echo "   rm -rf $HOME/.conda"
+echo "   rm -rf $HOME/.condarc $HOME/.continuum $HOME/.conda"
 echo
 echo "2 - Edit your file $HOME/.bashrc to remove the following lines: "
 grep _PYTHON_TEAM_DIR $HOME/.bashrc
